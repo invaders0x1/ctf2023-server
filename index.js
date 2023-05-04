333099902d8fe05739be8581ca246aa01fbc0b54
Farmansk <shkfrmn786@gmail.com>
2023-05-04 12:08:46 +0530

    ready

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 5000;
 
 app.use(cors());
-// app.use(express.static(path.join(__dirname + "/public")));
+app.use(express.static(path.join(__dirname + "/public")));
 app.use(express.json())
 
 // Available Routes
